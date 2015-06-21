8c5a2334ffe3961c5c45d93881bcd8d0647422dd
elixic <elixic@gmail.com>
2015-06-21 00:35:57 -0700

    fixing some formatting.

--- server/main.js
+++ server/main.js
@@ -2,18 +2,19 @@
 
 var express = require('express'),
     log = require('debug')('app:main'),
-    app = express();
+    app = express(),
+    server, io;
 
 app.use(express.static('public'));
 
-var server = app.listen(9001, function() {
+server = app.listen(9001, function() {
     var host = server.address().host,
         port = server.address().port;
 
     log("services app listening at http://" + host + ":" + port);
 });
 
-var io = require('socket.io')(server);
+io = require('socket.io')(server);
 
 io.on('connection', function(socket){
     console.log('a user connected');
